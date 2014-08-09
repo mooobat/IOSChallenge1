561abb87dd140c8daea3be598b5a4d0567f7b274
david batic <dbatic@MacBook-Pro.local>
2014-08-08 20:39:13 -0400

    Initial Commit

---
+++ IOSChallenge1/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  IOSChallenge1
+//
+//  Created by david batic on 8/8/14.
+//  Copyright (c) 2014 moobat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
